98266316edb35004c4bf578fca0edc7d2e360812
Pavlo Kuraksin <kuraksinpavel@gmail.com>
2023-08-29 15:19:15 +0300

    add sequelize and corse

---
+++ src/model/productModel.js
@@ -0,0 +1,57 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../config/dataBase.js';
+export class Product extends Model {}
+
+Product.init(
+  {
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    itemId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    fullPrice: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    screen: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capacity: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ram: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Product',
+    tableName: 'products',
+  }
+);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,33 @@
-/* eslint-disable no-console */
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import { Product } from "./model/productModel";
+
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+app.use(cors())
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/products', async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const perPage = 5;
+  const offset = (page - 1) * perPage;
+
+  try {
+    const productsOnPage = await Product.findAll({
+      offset,
+      limit: perPage,
+    });
+
+    res.status(200).json(productsOnPage);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 });
 
 app.listen(port, () => {
