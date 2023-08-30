750c796ae691580b70e0238117f72a5098ad2262
Pavlo Kuraksin <kuraksinpavel@gmail.com>
2023-08-30 18:29:53 +0300

    add working postgreSQL

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import Product from './model/Product';
 import { sequelize } from './sequelize';
+import productsData from './api/phones.json';
 
 const app = express();
 const PORT = process.env.PORT || 5432;
@@ -23,6 +24,18 @@ Product.sync({ force: true })
     .catch((err: any) => {
         console.error('Unable to create product table:', err);
     });
+
+(async () => {
+    try {
+        await sequelize.sync();
+        await Product.bulkCreate(productsData);
+        console.log('Data loaded successfully');
+    } catch (error) {
+        console.error('Error loading data:', error);
+    } finally {
+        await sequelize.close();
+    }
+})();
 
 app.get('/', (req: any, res: any) => {
     res.send('Hello World!');
