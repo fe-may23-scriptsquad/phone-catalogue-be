be358be45332b3b41a6ac9c16788b772c27e578b
Pavlo Kuraksin <kuraksinpavel@gmail.com>
2023-08-30 18:51:33 +0300

    іÑsmall fix

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize(
-    'postgres://pasha:wFQoGnoFQarCHOn5vGOYiUMuYdkqKyJE@dpg-cjmq3d4dfrcc73b2ot7g-a.frankfurt-postgres.render.com/product_db_ll9c', // URL подключения
+    'postgres://pasha:wFQoGnoFQarCHOn5vGOYiUMuYdkqKyJE@dpg-cjmq3d4dfrcc73b2ot7g-a.frankfurt-postgres.render.com/product_db_ll9c',
     {
         dialect: 'postgres',
         dialectOptions: {
